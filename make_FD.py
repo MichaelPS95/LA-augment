be3787d4ac29be58750e1b4dfc3b823271a7f3c1
Michael Poirier-Shelton <85369795+MichaelPS95@users.noreply.github.com>
2023-12-07 14:20:01 -0700

    Add files via upload

---
+++ make_FD.py
@@ -0,0 +1,42 @@
+import sys
+import re
+
+def make_data_file(LA, params, output):
+    design = []
+    with open(LA,"r") as f:
+        for line in f:
+            l = line.strip().split()
+            design.append(l)
+    # print(design)
+    num_params = 0
+    param_list = []
+    with open(params, "r") as f:
+        num_params = int(f.readline().strip())
+        line = f.readline().strip().split()
+        for num in line:
+            param_list.append(int(num))
+        # print(num_params)
+        # print(param_list)
+    
+    alphabet_size = 26
+
+    with open(output, "w") as f:
+        f.write(str(num_params) + "\n")
+        for i in range(num_params):
+            letter = chr(ord('A') + (i) % alphabet_size)
+            number = i // alphabet_size + 1
+            result = f"{letter}{number}"
+            line = ""
+            line = line + result + "\t" + str(param_list[i]) + "\t0\t"
+            for j in range(param_list[i]):
+                line += str(j) + "\t"
+            if(i == num_params-1):
+                f.write(line)
+            else:
+                f.write(line+"\n")
+
+
+for i in range(1):
+    s1 = "/home/michael/Desktop/Designs/2_2_2_2_factor/optimal/0_optimal.tsv"
+    s2 = "/home/michael/Desktop/Designs/2_2_2_2_factor/optimal/FD.tsv"
+    make_data_file(s1 , f"/home/michael/Desktop/Designs/2_2_2_2_factor/2_2_2_2_params.tsv", s2)
